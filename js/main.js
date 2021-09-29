214d78216a90d04f997efa3d6c18d41d74cfda2c
Artem Pavlov <rtmpvlv@gmail.com>
2021-09-29 11:25:23 +0300

    Fixed things

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // import { photoDescriptions } from './photo-description.js'
 import { getPhotoDescription } from './photo-description.js'
-import { createPicture } from './draw-thumbnails.js'
+import { createPicture } from './thumbnails.js'
 
 // console.log(photoDescriptions);
 // console.log(getComments());
